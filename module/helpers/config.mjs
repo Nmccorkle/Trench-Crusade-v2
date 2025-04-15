64696e4491ba783f4c50bbdb5b88c576cbe61f78
Nmccorkle <nmccorkle@lesos.io>
2025-04-14 20:46:29 -0500

    TC testing 1

---
+++ module/helpers/config.mjs
@@ -0,0 +1,59 @@
+export const BOILERPLATE = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+BOILERPLATE.abilities = {
+  str: 'BOILERPLATE.Ability.Str.long',
+  agi: 'BOILERPLATE.Ability.Agi.long',
+  end: 'BOILERPLATE.Ability.End.long',
+  wil: 'BOILERPLATE.Ability.Wil.long',
+  int: 'BOILERPLATE.Ability.Int.long',
+  dev: 'BOILERPLATE.Ability.Dev.long',
+  cor: 'BOILERPLATE.Ability.Cor.long',
+  bs: 'BOILERPLATE.Ability.Bs.long',
+  ws: 'BOILERPLATE.Ability.Ws.long',
+};
+
+BOILERPLATE.abilityAbbreviations = {
+  str: 'BOILERPLATE.Ability.Str.abbr',
+  agi: 'BOILERPLATE.Ability.Agi.abbr',
+  end: 'BOILERPLATE.Ability.End.abbr',
+  wil: 'BOILERPLATE.Ability.Wil.abbr',
+  int: 'BOILERPLATE.Ability.Int.abbr',
+  dev: 'BOILERPLATE.Ability.Dev.abbr',
+  cor: 'BOILERPLATE.Ability.Cor.abbr',
+  bs: 'BOILERPLATE.Ability.Bs.abbr',
+  ws: 'BOILERPLATE.Ability.Ws.abbr',
+};
+
+BOILERPLATE.skills = {
+  alchemy: 'BOILERPLATE.Skills.Alchemy.long',
+  athletics: 'BOILERPLATE.Skills.Athletics.long',
+  awareness: 'BOILERPLATE.Skills.Awareness.long',
+  brawling: 'BOILERPLATE.Skills.Brawling.long',
+  command: 'BOILERPLATE.Skills.Command.long',
+  discipline: 'BOILERPLATE.Skills.Discipline.long',
+  drive: 'BOILERPLATE.Skills.Drive.long',
+  esoterica: 'BOILERPLATE.Skills.Esoterica.long',
+  faith: 'BOILERPLATE.Skills.Faith.long',
+  forgecraft: 'BOILERPLATE.Skills.Forgecraft.long',
+  gambling: 'BOILERPLATE.Skills.Gambling.long',
+  heresy: 'BOILERPLATE.Skills.Heresy.long',
+  intimidation: 'BOILERPLATE.Skills.Intimidation.long',
+  investigation: 'BOILERPLATE.Skills.Investigation.long',
+  language: 'BOILERPLATE.Skills.Language.long',
+  lore: 'BOILERPLATE.Skills.Lore.long',
+  medicine: 'BOILERPLATE.Skills.Medicine.long',
+  navigation: 'BOILERPLATE.Skills.Navigation.long',
+  riding: 'BOILERPLATE.Skills.Riding.long',
+  sabotage: 'BOILERPLATE.Skills.Sabotage.long',
+  stealth: 'BOILERPLATE.Skills.Stealth.long',
+  subterfuge: 'BOILERPLATE.Skills.Subterfuge.long',
+  survival: 'BOILERPLATE.Skills.Survival.long',
+  tactics: 'BOILERPLATE.Skills.Tactics.long',
+  torture: 'BOILERPLATE.Skills.Torture.long',
+  tradecraft: 'BOILERPLATE.Skills.Tradecraft.long',
+  zealotry: 'BOILERPLATE.Skills.Zealotry.long',
+};
